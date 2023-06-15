9554968f39af244d8c87b0143489842d287f5667
ok-olly <jeongoklee0108@gmail.com>
2023-06-16 00:13:24 +0900

    add sidebar

--- js/locations.js
+++ js/locations.js
@@ -3,71 +3,82 @@ export const locations = {
     latitude: 53.342461028712805,
     longitude: -6.281377866857469,
     name: "St. Catherine's Park",
+    description: 'Park exteriors',
+    address: "St. Catherine's Park, Hanbury Lane, Dublin, Ireland",
   },
 
   dunLaoghaireHabourEast: {
     latitude: 53.298498679178834,
     longitude: -6.124278344257477,
     name: 'Dún Laoghaire Harbour East',
+    description:
+      'Site of "A Beautiful Sea" video and where Conor returns to look across the sea to Britain after Raphina has left.',
+    address:
+      'Dún Laoghaire Harbour East, Dún Laoghaire-Rathdown, County Dublin, Ireland',
   },
 
   coliemoreHarbour: {
     latitude: 53.27502705763972,
     longitude: -6.093855758349382,
     name: 'Coliemore Harbour',
+    description: '',
+    address: 'Coliemore Harbour, County Dublin, Ireland',
   },
 
   dalkeyIsland: {
     latitude: 53.27185869576267,
     longitude: -6.0843660542747084,
     name: 'Dalkey Island',
+    description: '',
+    address: 'Dalkey Island, Dublin, County Dublin, Ireland',
   },
 
   sanctaMariaPrimarySchool: {
     latitude: 53.3333566632413,
     longitude: -6.267131365560957,
     name: 'Sancta Maria Primary School',
+    description: 'First sight of Raphina from the entrance here',
+    address: 'Sancta Maria Primary School, Synge Street, Dublin, Ireland',
   },
 
   fuscosCafe: {
     latitude: 53.34106514813436,
     longitude: -6.278711411537048,
     name: "Fusco's Cafe",
+    description: 'Cafe scene after filming "The Riddle of the Model"',
+    address: "Fusco's Cafe, 27 Meath Street, Merchants Quay, Dublin, Ireland",
   },
 
   frederickStreetNorth: {
     latitude: 53.35538097656439,
     longitude: -6.264464549613137,
     name: 'Frederick Street North',
+    description:
+      'Site of Conor and Raphina\'s coversation as he "spins" her home - site of DeafHear.ie anachronism',
+    address: 'Frederick Street North, Rotunda, Dublin, Ireland',
   },
 
   greenleaRoad143: {
     latitude: 53.30719453579432,
     longitude: -6.299668297150399,
     name: '143 Greenlea Road',
+    description: "Eamon's house, where the band practice",
+    address: '143 Greenlea Road, Dublin, Ireland',
   },
 
   syngeStreetCBS: {
     latitude: 53.33359751220071,
     longitude: -6.267859429588321,
     name: 'Synge Street CBS',
+    description: 'School exterior',
+    address: 'Synge Street CBS, Synge Street, Dublin, Ireland',
   },
 
   syngeStreet40: {
     latitude: 53.33323980850529,
     longitude: -6.266782271270282,
     name: '40 Synge Street',
+    description: "Exterior of Raphina's home",
+    address: "40 Synge Street, Saint Kevin's, Dublin, Ireland",
   },
-
-  // place : [latitude, longitude]
-  // syngeStreetCBS: [53.33359751220071, -6.267859429588321],
-  // stCatherinesPark: [53.342461028712805, -6.281377866857469],
-  // dunLaoghaireHabourEast: [53.298498679178834, -6.124278344257477],
-  // coliemoreHarbour: [53.27502705763972, -6.093855758349382],
-  // dalkeyIsland: [53.27185869576267, -6.0843660542747084],
-  // sanctaMariaPrimarySchool: [53.3333566632413, -6.267131365560957],
-  // fuscosCafe: [53.34106514813436, -6.278711411537048],
-  // frederickStreetNorth: [53.35538097656439, -6.264464549613137],
-  // SyngeStreet40: [53.33323980850529, -6.266782271270282],
-  // GreenleaRoad143: [53.30719453579432, -6.299668297150399],
 };
---
+++ js/script.js
@@ -0,0 +1,98 @@
+// locations info => https://www.imdb.com/title/tt3544112/locations/
+import { locations } from './locations.js';
+const h1 = document.querySelector('.heading');
+
+class App {
+  #map;
+
+  constructor() {
+    this._loadMap();
+    Object.values(locations).map(location => {
+      this._createMarkers(location);
+      this._renderList(location);
+    });
+  }
+
+  _loadMap() {
+    this.map = L.map('map').setView(
+      [locations.syngeStreet40.latitude, locations.syngeStreet40.longitude],
+      12
+    );
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map);
+  }
+
+  _createMarkers(location) {
+    L.marker([location.latitude, location.longitude])
+      .addTo(this.map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+        })
+      )
+      .setPopupContent(location.name)
+      .openPopup();
+  }
+
+  _renderList(location) {
+    const html = `
+    <li class="placelist">
+      <img
+        class="placelist__image"
+        src="https://picsum.photos/200"
+        alt="movie photo"
+      />
+      <h2 class="placelist__title">${location.name}</h2>
+      <span class="placelist__address">${location.address}</span>
+      <p class="placelist__description">${location.description}</p>
+      <button class="placelist__button">visited</button>
+    </li>`;
+
+    h1.insertAdjacentHTML('afterbegin', html);
+  }
+}
+
+const app = new App();
+
+// const markers = (latitude, longitude, content) => {
+//   L.marker([latitude, longitude])
+//     .addTo(map)
+//     .bindPopup(
+//       L.popup({
+//         maxWidth: 250,
+//         minWidth: 100,
+//       })
+//     )
+//     .setPopupContent(content)
+//     .openPopup();
+// };
+
+// Object.values(locations).map(location => {
+//   markers(location.latitude, location.longitude, location.name);
+// });
+
+// markers(
+//   locations.syngeStreetCBS.latitude,
+//   locations.syngeStreetCBS.longitude,
+//   'Synge Street CBS'
+// );
+
+// var marker = L.marker([
+//   locations.syngeStreetCBS[0],
+//   locations.syngeStreetCBS[1],
+// ])
+//   .addTo(map)
+//   .bindPopup(
+//     L.popup({
+//       maxWidth: 250,
+//       minWidth: 100,
+//       autoClose: false,
+//     })
+//   )
+//   .setPopupContent('Synge Street CBS')
+//   .openPopup();
